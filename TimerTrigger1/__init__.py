0a18f380e07936929487024021195c50865e0e1d
nicolaiskat <jegerrockstar@gmail.com>
2023-05-21 03:33:33 +0200

    comment out integration with discord

--- TimerTrigger1/__init__.py
+++ TimerTrigger1/__init__.py
@@ -11,11 +11,11 @@ async def main(mytimer: func.TimerRequest) -> None:
     utc_timestamp = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
     
-    async with aiohttp.ClientSession() as session:
+    """ async with aiohttp.ClientSession() as session:
         url = 'https://discord.com/api/webhooks/1109643464481714188/7Lw0nJm9MeSiFQMlqo9-2h1eINuhIQ3irqIxWFn59Ue3ewRCFau7mnu_qbVrt3Xz9W__'
         webhook = Webhook.from_url(url, session=session)
         embed = discord.Embed(title='This is a title')
-        await webhook.send(embed=embed)
+        await webhook.send(embed=embed) """
             
     if mytimer.past_due:
         logging.info('The timer is past due!')
