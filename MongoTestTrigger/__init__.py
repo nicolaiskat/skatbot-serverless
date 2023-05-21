1ca1c2c7884f7053aead80734d145faade625c6b
nicolaiskat <jegerrockstar@gmail.com>
2023-05-22 01:40:53 +0200

    Update __init__.py

--- MongoTestTrigger/__init__.py
+++ MongoTestTrigger/__init__.py
@@ -2,6 +2,7 @@ import logging
 from pymongo import MongoClient
 import os
 from mongobot.dbs.players import getPlayers
+import aiohttp
 
 import azure.functions as func
 
@@ -10,8 +11,8 @@ async def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Get dabase')
     uri = os.environ['MONGO_URI']
     client = MongoClient(uri)
-    dbname = client[os.environ['CSGOSTATS_DATABASE']]
-    collection = dbname["players"]
-    players = getPlayers()
-    playerName = players[0]['name']
+    
+    async with aiohttp.ClientSession() as session:
+        players = getPlayers()
+        playerName = players[0]['name']
     return func.HttpResponse(f"Player name: {playerName}", status_code=200)
